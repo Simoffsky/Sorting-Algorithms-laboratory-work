5825828be67e1d390406aff524e38e9cb5fa7c57
Simoffsky <maks-kirsanov26@mail.ru>
2022-05-22 19:36:24 +1000

    added bubble sort with flag

--- main.cpp
+++ main.cpp
@@ -7,7 +7,30 @@
 using namespace std;
 
 
+//Распечатка массива с консоль
+void print(vector<int>& arr) {
+	for (int i = 0; i < arr.size(); cout << arr[i++] << ' ');
+	cout << endl;
+}
+
+//Сортировка пузырьком с флажком
+void bubbleSortFlag(vector<int>& arr) {
+	bool flag;
+	for (int i = 0; i < arr.size(); ++i) {
+		flag = true;
+		for (int j = 0; j < arr.size() - 1 - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				flag = false;
+			}
+		}
+		if (flag)
+			return;
+	}
+}
 
 int main(void) {
-	cout << "Hello!";
+	vector<int> arr = {5,4,3,2,10,40};
+	bubbleSortFlag(arr);
+	print(arr);
 }
